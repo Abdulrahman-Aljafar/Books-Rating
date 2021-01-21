2d6cced0d5367231eda022590b88ff5f54577d35
Manal Alghamdi <manalalghamdi0@gmail.com>
2021-01-21 20:15:31 +0300

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Signup from "./register/Signup";
+import Login from "./register/Login";
 
 function App() {
+  const [selectMovie, setSelectMovie] = useState({});
+  const [dataLoading, setDataloading] = useState(false)
+  const [auth, setAuth] = useState({ currentUser: null, isLoggedIn: false });
+  const userLogin = () => {
+    if (localStorage.jwtToken) {
+      const jwtToken = localStorage.jwtToken;
+      const currentUser = jwt_decode(jwtToken, "SECRET").user;
+      setAuth({ currentUser, isLoggedIn: true });
+    } else {
+      setAuth({ currentUser: null, isLoggedIn: false });
+    }
+
+    setDataloading(true)
+    console.log("The current User is: ", auth.currentUser);
+  };
+  useEffect(userLogin, []);
   return (
     <>
-    
+     { dataLoading &&
+        <Router>
+        
+          <Route path="/login">
+            <Login loginCallback={userLogin} />
+          </Route>
+
+          <Route path="/signup">
+            <Signup loginCallback={userLogin} />
+          </Route>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+
+        </Router>
+      }
     </>
   );
 }
