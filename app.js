6bd87a90eaa21803dd5eb83c768aee553d01ba28
Manal Alghamdi <manalalghamdi0@gmail.com>
2021-01-27 13:04:24 +0300

    updated

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 var cors = require("cors");
 // var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+//jb
 const mongoose = require('mongoose')
 var app = express();
 app.use(cors());
